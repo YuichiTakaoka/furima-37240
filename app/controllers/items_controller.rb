43b6c067abca4d1fb3df80178e52676d56e64bec
YuichiTakaoka <impression96722697@gmail.com>
2021-12-15 18:46:08 +0900

    商品編集の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to item_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   def move_to_index
     unless user_signed_in?
